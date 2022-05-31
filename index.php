bb2ee5cc51c77529a7cccf90639b900a58e57ad5
abdulaziz1999 <azizmentor96@gmail.com>
2022-05-31 08:36:44 +0700

    landing fix

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             </li>
           </ul>
           <a
-           href="<?= $base_url?>/psb/admin/"
+           href="<?= $base_url?>/psb/admin"
             id="navAction"
             class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
           >
@@ -436,7 +436,7 @@
                 .
               </p>
               <div class="w-full font-bold text-xl text-gray-800 px-6">
-                Visi Misi Yang Bakti Amal
+                Visi Misi Yayasan Bakti Amal
                 <br>
               </div>
               <p class="text-gray-800 text-base px-6 mb-5">
